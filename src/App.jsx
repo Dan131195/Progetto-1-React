f851e00f85e28dba24f78d216a918e0828ab0e4d
Dan131195 <daniele.franceschini1395@gmail.com>
2025-01-12 23:50:52 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,15 @@ function App() {
   return (
     <>
       <MyNavBar />
-      <h4>Star Wars</h4>
+      <h4 className="text-white titleSaga">Star Wars</h4>
       <Movie movie="Star Wars" />
-      <h4>Harry Potter</h4>
+      <h4 className="text-white titleSaga">Harry Potter</h4>
       <Movie movie="Harry Potter" />
-      <h4>Lord of The Rings</h4>
+      <h4 className="text-white titleSaga">Lord of The Rings</h4>
       <Movie movie="Lord of the rings" />
-      <h4>Naruto</h4>
+      <h4 className="text-white titleSaga">Naruto</h4>
       <Movie movie="Naruto" />
-      <h4>One Piece</h4>
+      <h4 className="text-white titleSaga">One Piece</h4>
       <Movie movie="One Piece" />
 
       <hr className="text-white border-3" />
